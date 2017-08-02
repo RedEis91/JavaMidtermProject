638e91185f79b38fc012ec6f9c63e52e080a65a9
RedEis91 <imitationkrabs91@gmail.com>
2017-08-02 12:01:10 -0400

    populated arraylist within catalog

--- src/Catalog.java
+++ src/Catalog.java
@@ -4,15 +4,37 @@ import java.io.*;
 public class Catalog
 {
     //constructor for libraryItem list // catalog
-    ArrayList<libraryItem> list = new ArrayList<libraryItem>();
+
+
+    public static ArrayList<libraryItem> readFromFile(File books) {
+        ArrayList<libraryItem> list = new ArrayList<libraryItem>();
+        //FileReader always needs to be wrapped in Try/Catch.
+        try {
+            // to use buffered reader, you have to instantiate a reader first
+            FileReader reader = new FileReader(books);
+            //BufferedReader (more specific file reader) has method called readLine() to read data stream line by line
+            BufferedReader bufReader = new BufferedReader(reader);
+            String line = bufReader.readLine();
+            //ensure the line being fed into bufReader isn't null
+            while (line != null) {
+                System.out.println(line);
+                line = bufReader.readLine(); // read the next line
+            }
+            bufReader.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("File not found!");
+        }
+        return list;
+    }
 
 
     //method to add libraryItem to List
-    public void addLibraryItem(String authorLastName, String authorFirstName, String bookTitle, CheckedOut check, LocalDate date)
-    {
-        libraryItem a = new libraryItem(authorLastName, authorFirstName, bookTitle, check, date);
-        list.add(a);
-    }
+//    public void addLibraryItem(String authorLastName, String authorFirstName, String bookTitle, CheckedOut check, LocalDate date)
+//    {
+//        libraryItem a = new libraryItem(authorLastName, authorFirstName, bookTitle, check, date);
+//        list.add(a);
+//    }
 
 
 }
--- src/Library.java
+++ src/Library.java
@@ -9,52 +9,24 @@ public class Library
 {
     public static void main(String[] args)
     {
-
-
-        String lastName;
-        String title;
-        String firstName;
-        CheckedOut checkedOut;
-        LocalDate localDate = LocalDate.now(ZoneId.systemDefault());
-        localDate = localDate.plus(14, ChronoUnit.DAYS);
-        String enume;
-
-
-        Scanner scan = new Scanner(System.in);
-        //int keepGoing;
         BooksFile booksFile = new BooksFile();
         File books = booksFile.books;
-//        ArrayList<libraryItem> testList = booksFile.readFromFile(books);
-//        int i = 1;
-//        for (libraryItem book: testList) {
+
+
+
+        ArrayList<libraryItem> list = Catalog.readFromFile(books);
+//        String lastName;
+//        String title;
+//        String firstName;
+//        CheckedOut checkedOut;
+//        LocalDate localDate = LocalDate.now(ZoneId.systemDefault());
+//        localDate = localDate.plus(14, ChronoUnit.DAYS);
+//        String enume;
 //
-//            System.out.println(""+ i+ ": " + book);
-//            i++;
-//        }
+//
+//        Scanner scan = new Scanner(System.in);
+//        //int keepGoing;
 
-        System.out.println("Please enter first name: ");
-        firstName = scan.nextLine();
 
-        System.out.println("Please enter last name: ");
-        lastName = scan.nextLine();
-
-        System.out.println("Please enter the title: ");
-        title = scan.nextLine();
-
-        System.out.println("Is this book checked out: ");
-        enume = scan.nextLine();
-
-        if(enume.contains("Y") || enume.contains("y"))
-        {
-            checkedOut = CheckedOut.YES;
-        }
-        else
-        {
-            checkedOut = CheckedOut.NO;
-        }
-
-            libraryItem it = new libraryItem(lastName, firstName, title, checkedOut, localDate);
-        System.out.println("Your new book is: " + it);
-        BooksFile.writeToFile(it, books);
     }
 }
