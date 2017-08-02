e40ad99a5eb3167cf8ad9500ef667223f42a4519
RedEis91 <imitationkrabs91@gmail.com>
2017-08-02 17:00:39 -0400

    started menu

--- src/Library.java
+++ src/Library.java
@@ -14,33 +14,58 @@ public class Library
         Scanner scan = new Scanner(System.in);
         Catalog booksFile = new Catalog();
         File books = new File("books.txt");
+        int selection;
+
 
         ArrayList<libraryItem> booksList = new ArrayList<libraryItem>();
         ArrayList<libraryItem> list = Catalog.readFromFile(books, booksList);
         ArrayList<libraryItem> onShelf = Catalog.displayOnShelf(list);
         ArrayList<libraryItem> checkedOut = Catalog.displayCheckedOut(list);
+        System.out.println("WELCOME TO THE LIBRARY!");
+        System.out.println("1) VIEW ALL BOOKS");
+        System.out.println("2) VIEW BOOKS IN STOCK");
+        System.out.println("3) VIEW CHECKED OUT BOOKS");
+        selection = scan.nextInt();
+
+            if (selection == 1) {
+                for (libraryItem libraryitem: list) {
+                    System.out.println(libraryitem);
+                }
+            }
+                    else if
+            (selection == 2) {for (libraryItem libraryitem: onShelf) {
+                    System.out.println(libraryitem);
+                }}
+                     else if
+            (selection == 3) {
+                for (libraryItem libraryitem: checkedOut) {
+                    System.out.println(libraryitem);
+                } }
+                else {
+                    System.out.println("1 2 or 3 please");
+            }
 
 
-        System.out.println("Books checked out " + checkedOut.size());
-        for (libraryItem item: checkedOut) {
-            System.out.println(item);
-        }
+//        System.out.println("Books checked out " + checkedOut.size());
+//        for (libraryItem item: checkedOut) {
+//            System.out.println(item);
+//        }
 
-        System.out.println("Return?");
-        String retu;
-        retu = scan.nextLine();
-        libraryItem ite = checkedOut.get(0);
-        ite.placeOnShelf(retu);
-        onShelf = Catalog.displayOnShelf(list);
-
-
-        Catalog.writeToFile(list, books);
-
-
-        System.out.println("Books on shelf " + onShelf.size());
-        for (libraryItem item: onShelf) {
-            System.out.println(item);
-        }
+//        System.out.println("Return?");
+//        String retu;
+//        retu = scan.nextLine();
+//        libraryItem ite = checkedOut.get(0);
+//        ite.placeOnShelf(retu);
+//        onShelf = Catalog.displayOnShelf(list);
+//
+//
+//        Catalog.writeToFile(list, books);
+//
+//
+//        System.out.println("Books on shelf " + onShelf.size());
+//        for (libraryItem item: onShelf) {
+//            System.out.println(item);
+//        }
 
 
 
